ef3d252034916267eff09b907be7d15499da83b6
Falsis <63756985+falsisdev@users.noreply.github.com>
2021-07-22 15:41:13 +0300

    create: Created main file

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+import(
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"strings"
+)
+func main() {
+	fmt.Print("Proje Başlatıldı\n")
+	e := echo.New()
+	e.GET("/test", func(c echo.Context) error{
+		return c.String(http.StatusOK, "Slm Dünya")
+	})
+	e.GET("/", func(c echo.Context) error{
+		return c.File("index.json")
+	})
+	e.GET("/owofy", func(c echo.Context) error{
+		if c.QueryParam("message") == "" {
+			return c.File("./hata/owofy.json")
+		}
+		owo := strings.Replace(strings.Replace(c.QueryParam("message"), "l", "w", -1), "r", "w", -1)
+		return c.JSON(http.StatusOK, owo)
+	})
+	e.Logger.Fatal(e.Start(":3000"))
+}
